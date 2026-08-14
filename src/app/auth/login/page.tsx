0b063e8e9d40b4013ee4f4bf7483ec6fc6d53c0f
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-14 21:34:06 +0600

    login page design done

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,10 @@
+import { LoginCard } from '@/components/shared/LoginCard';
 import React from 'react';
 
 const LoginPage = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center items-center min-h-screen w-11/12 mx-auto'>
+            <LoginCard/>
         </div>
     );
 };
--- src/components/shared/LoginCard.tsx
+++ src/components/shared/LoginCard.tsx
@@ -12,69 +12,55 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
 
-export function SignUpCard() {
+export function LoginCard() {
     return (
         <Card className="w-full max-w-sm">
             <CardHeader>
-                <CardTitle>Create an account</CardTitle>
+                <CardTitle>Login to your account</CardTitle>
                 <CardDescription>
-                    Enter your details below to create your account
+                    Enter your email below to login to your account
                 </CardDescription>
                 <CardAction>
-                    <Link href='/auth/login'>Log In</Link>
+                    <Link href='/auth/login'>Sign Up</Link>
                 </CardAction>
             </CardHeader>
             <CardContent>
                 <form>
                     <div className="flex flex-col gap-6">
                         <div className="grid gap-2">
-                            <Label htmlFor="name">Name</Label>
-                            <Input
-                                id="name"
-                                type="text"
-                                placeholder="your full name"
-                                required
-                            />
-                        </div>
-
-                        <div className="grid gap-2">
                             <Label htmlFor="email">Email</Label>
                             <Input
                                 id="email"
                                 type="email"
-                                placeholder="your@example.com"
+                                placeholder="you@example.com"
                                 required
                             />
                         </div>
-
                         <div className="grid gap-2">
                             <div className="flex items-center">
                                 <Label htmlFor="password">Password</Label>
-                                <Link
-                                    href="/"
+                                <a
+                                    href="#"
                                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                                 >
                                     Forgot your password?
-                                </Link>
+                                </a>
                             </div>
                             <Input
+                            placeholder="Enter your password"
                                 id="password"
                                 type="password"
-                                placeholder="least 8 characters"
-                                required
-                                pattern="(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}"
-                                title="Must contain at least 8 characters, including letters, numbers, and special characters."
-                            /> 
+                                required />
                         </div>
                     </div>
                 </form>
             </CardContent>
             <CardFooter className="flex-col gap-2">
                 <Button type="submit" className="w-full">
-                    Sign Up
+                    Login
                 </Button>
                 <Button variant="outline" className="w-full">
-                    Sign Up with Google
+                    Login with Google
                 </Button>
             </CardFooter>
         </Card>
